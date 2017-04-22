7051311954681001c19aaba261bbd357d004f9c1
Kelene Chung <simoy93@gmail.com>
2017-04-21 22:30:51 -0400

    update js file

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,7 @@
 $(function(){
     $(".button_container").click(function(){
     	{
-    	window.location.href = "http://localhost:3000/games";
+    	window.location.href = "games";
 		}
     });
 });
